84442315cdab01a734c61e3d2bc4791b590de7f3
free__loop <1696905265@qq.com>
2019-07-06 17:38:46 +0800

    会员积分-时间设置

--- src/views/Hyjf/Sjsz/index.vue
+++ src/views/Hyjf/Sjsz/index.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-    时间设置
+  <div class="sjsz">
+    <div class="spgl-item">
+      <div class="spgl-item--title">积分任务设置</div>
+      <div class="spgl-item--content content1">
+        <div class="set-time" v-show='!editTimeShow'>
+          <p class="row"><span class="row-name">确认收货周期：</span>{{time.qrsh}}天</p>
+          <p class="row"><span class="row-name">未付款周期：</span>{{time.wfk}}天</p>
+          <p class="row"><span class="row-name">会员周期：</span>{{time.hy}}天</p>
+          <p class="row"><span class="row-name">积分清空周期：</span>{{time.jfqk}}天</p>
+          <p class="row"><span class="row-name">邀请码更新周期：</span>{{time.yqm}}天</p>
+        </div>
+        <div class="set-time" v-show='editTimeShow'>
+          <p class="row"><span class="row-name">确认收货周期：</span><el-input v-model="editTime.qrsh" placeholder="请输入数字"></el-input>（单位：天）</p>
+          <p class="row"><span class="row-name">未付款周期：</span><el-input v-model="editTime.wfk" placeholder="请输入数字"></el-input>（单位：天）</p>
+          <p class="row"><span class="row-name">会员周期：</span><el-input v-model="editTime.hy" placeholder="请输入数字"></el-input>（单位：天）</p>
+          <p class="row"><span class="row-name">积分清空周期：</span><el-input v-model="editTime.jfqk" placeholder="请输入数字"></el-input>（单位：天）</p>
+          <p class="row"><span class="row-name">邀请码更新周期：</span><el-input v-model="editTime.yqm" placeholder="请输入数字"></el-input>（单位：天）</p>
+        </div>
+        <div class="edit" v-show='!editTimeShow'>
+          <el-button size="mini" class="success-btn" @click="editTimeFn">编辑</el-button>
+        </div>
+        <div class="edit" v-show='editTimeShow'>
+          <el-button size="mini" class="success-btn" @click="saveTimeFn">保存</el-button>
+          <el-button size="mini" @click="cancelTimeFn">取消</el-button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script src="./script.js"></script>
--- src/views/Hyjf/Sjsz/script.js
+++ src/views/Hyjf/Sjsz/script.js
@@ -2,7 +2,32 @@ export default {
   name: 'hyjf',
   data () {
     return {
-      // ..
+      editTimeShow: false,
+      time: {qrsh: 10, wfk: 7, hy: 30, jfqk: 30, yqm: 30},
+      editTime: {qrsh: 10, wfk: 7, hy: 30, jfqk: 30, yqm: 30}
+    }
+  },
+  methods: {
+    editTimeFn () {
+      Object.assign(this.editTime, this.time)
+      this.editTimeShow = true
+    },
+    saveTimeFn () {
+      if (this.verify()) return this.$message.error('不能为空值')
+      Object.assign(this.time, this.editTime)
+      this.editTimeShow = false
+    },
+    cancelTimeFn () {
+      Object.assign(this.editTime, this.time)
+      this.editTimeShow = false
+    },
+    verify () {
+      for (const key in this.editTime) {
+        if (this.editTime.hasOwnProperty(key)) {
+          const element = this.editTime[key]
+          if (!element) return true
+        }
+      }
     }
   }
 }
