38c8ff9736aadbd30121cdf8dccd3a6ffc35ac3e
likamoer <li_hx214@163.com>
2019-12-08 15:12:10 +0800

    commit

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -4,7 +4,7 @@
     <div class="login-form">
       <el-input v-model="userForm.account" placeholder="用户名" prefix-icon="el-icon-user"></el-input>
       <el-input v-model="userForm.password" placeholder="密码" type="password" show-password prefix-icon="el-icon-lock"></el-input>
-      <el-button type="danger" @click="submitUser" class="login-btn">登陆</el-button>
+      <el-button type="danger" @click="submitUser" @keydown.enter="submitUser" class="login-btn">登陆</el-button>
     </div>
   </div>
 </template>
