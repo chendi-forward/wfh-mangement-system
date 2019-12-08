64547278b2265cae4f4be746051150b34fb5d86d
panxuesen <13552198363@163.com>
2019-12-08 14:36:15 +0800

    merge dev

--- src/api/Shgl.js
+++ src/api/Shgl.js
@@ -1,1 +1,38 @@
 import {post, get, patch, put} from '@/api/http'
+
+// 获取所有的快递公司
+export function logisticsManage () {
+    return get('/after_sale/logistics_manage')
+}
+// Mark
+export function logisticsManage (data) {
+    return post('/after_sale/logistics_manage', data)
+}
+// 售后模块上面的头部部分
+export function saleHead () {
+    return get('/after_sale/sale_head')
+}
+// 订单列表
+export function orderList (data) {
+    return get('/after_sale/order_kind_list', data)
+}
+// 订单的详情
+export function orderDetail (data) {
+    return get('/after_sale/order_detail', data)
+}
+// 最新退款的数量
+export function newRefund () {
+    return get('/after_sale/new_refund')
+}
+// 新评价的数量
+export function newEvaluation () {
+    return get('/after_sale/new_evaluation')
+}
+// 获取订单下的所有的商品
+export function orderGoods (data) {
+    return get('/after_sale/get_order_goods', data)
+}
+// MARK 创建退款
+export function orderGoods (data) {
+    return post('/after_sale/get_order_goods', data)
+}
