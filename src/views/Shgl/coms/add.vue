3841580e2f322268753cdff0d6af7e9ee0988743
likamoer <li_hx214@163.com>
2019-12-21 20:36:53 +0800

    售后接口对接

--- src/views/Shgl/coms/add.vue
+++ src/views/Shgl/coms/add.vue
@@ -3,12 +3,12 @@
   <div class="tags-box">
     <div class="tag-box">
         <span class="header-title">物流公司:</span>
-        <el-select v-model="data.selectKey" @change='selectChange' placeholder="请选择">
+        <el-select v-model="selectKey" @change='selectChange' placeholder="请选择">
         <el-option
-            v-for="(item, index) in data.options"
+            v-for="(item, index) in logisticsData"
             :key="index"
-            :label="item.label"
-            :value="item.value">
+            :label="item.name"
+            :value="item.logistics_id">
         </el-option>
         </el-select>
     </div>
@@ -16,7 +16,7 @@
         <span class="header-title">退运单号:</span>
         <el-input
         placeholder="请输入单号..."
-        v-model="data.tydh">
+        v-model="tydh">
         </el-input>
     </div>
     <!-- <div class="tag-box">
@@ -27,18 +27,22 @@
         </el-input>
         （原路返回）
     </div> -->
-    <div class="tag-box">
+    <!-- <div class="tag-box">
         <span class="header-title">退款金额:</span>
         <el-input
         placeholder="请输入金额..."
         v-model="data.money">
         </el-input>
         （单位：元）
-    </div>
+    </div> -->
+    <div class="footer">
+      <el-button class="saveBtn" type="primary" @click.stop="sureSave">保 存</el-button>
+      <el-button class="cancleBtn" @click.stop="cancleSave">取 消</el-button></div>
     </div>
 </template>
 
 <script>
+import { logisticsManage } from 'API/Shgl'
   export default {
     props: {
       data: {
@@ -61,18 +65,66 @@
                 }]
             }
         }
-      }
+      },
     },
     data () {
       return {
+        selectKey: '',
+        tydh: '',
+        logisticsData: [],
       }
     },
     methods: {
+        sureSave (){
+          if (!this.selectKey || !this.tydh) {
+            this.$message({
+              message: '请正确填写表单信息',
+              type: 'warning'
+            });
+            return;
+          } else {
+            let obj = {
+              selectKey: this.selectKey,
+              tydh: this.tydh,
+              flag: 1
+            }
+            console.log('sureSave')
+            this.$emit('sure-save', obj)
+          }
+        },
+        cancleSave () {
+          console.log('cancleSave')
+          this.$emit('cancle-save')
+        },
+        getLogisticsData () {
+          logisticsManage().then(res => {
+            console.log(res, '==logisticsManage==')
+            if (res.message == 'ok') {
+              this.logisticsData = res.data
+            }
+          })
+        },
         selectChange () {}
+    },
+    mounted () {
+      this.getLogisticsData()
+      // console.log(this.goodsid, '=fooodwd==')
     }
   }
 </script>
 
-<style lang="less" scoped>
-
+<style lang="less">
+.footer {
+  text-align: center;
+}
+.el-button {
+      width: 70px;
+      height: 30px;
+      padding: 0;
+    }
+    .saveBtn {
+      background: #FF4B57;
+      color: #fff;
+      border: 0;
+    }
 </style>
--- src/views/Shgl/coms/receiving.vue
+++ src/views/Shgl/coms/receiving.vue
@@ -3,16 +3,16 @@
     <div class="tags-box">
         <div class="order-info">
             <p><span>订单编号:</span> {{data.ddbh}}</p>
-            <p><span>退运单号:</span> {{data.tkzh}}</p>
+            <p><span>退运单号:</span> {{data.tydh}}</p>
         </div>
         <div class="goods-info">
             <el-table
                 ref="multipleTable"
                 align='center'
-                :data="data.goods"
+                :data="goods_detail"
                 style="width: 100%">
                 <el-table-column
-                    prop="name"
+                    prop="goods_title"
                     align='left'
                     label="商品名称"
                     width="200">
@@ -21,18 +21,28 @@
                     prop="type"
                     align='left'
                     label="口味/规格">
+                    <template slot-scope="scope">
+                        <div class="taste">
+                            {{scope.row.taste}}、{{scope.row.standard}}
+                        </div>
+                    </template>
                 </el-table-column>
                 <el-table-column
-                    prop="num"
+                    prop="count"
                     align='center'
                     label="数量">
                 </el-table-column>
             </el-table>
         </div>
+        <div class="footer">
+        <el-button class="saveBtn" @click.stop="sureSave">确 认</el-button>
+        <el-button class="cancleBtn" @click.stop="cancleSave">取 消</el-button></div>
+        </div>
     </div>
 </template>
 
 <script>
+import { refundDetail, confirmReceipt } from 'API/Shgl'
   export default {
     props: {
       data: {
@@ -40,32 +50,83 @@
         default: () => {
             return {
                 ddbh: '1654966632',
-                tkzh: '5621654623',
-                goods: [
-                    {
-                        name: '考拉系列乳清蛋白粉',
-                        type: '巧克力，500g',
-                        num: 20
-                    }
-                ]
+                tydh: '5621654623',
             }
         }
+      },
+      goodsid: {
+          type: Number,
+          default: 0
       }
     },
     data () {
       return {
+        goods_detail: []
       }
     },
+    watch: {
+        goodsid (n) {
+            console.log(n, '===')
+            this.getDetailData()
+        }
+    },
     methods: {
-    }
+        getDetailData () {
+            let data ={
+                id: this.goodsid
+            }
+            refundDetail(data).then(res => {
+                console.log(res, '===detail')
+                if (res.message == 'ok') {
+                    this.goods_detail = res.data.goods_detail
+                }
+            })
+        },
+        sureSave(){
+            let obj ={
+                flag: 2
+            }
+            this.$emit('sure-save', obj)
+            let data = {
+                id: this.goodsid
+            }
+            confirmReceipt(data).then(res => {
+                this.$message({
+                    message: '确认收货成功!',
+                    type: 'success'
+                });
+                console.log(res, 'confirmreceipt====')
+            })
+        },
+        cancleSave() {
+            this.$emit('cancle-save')
+        }
+     },
+     created () {
+         this.getDetailData()
+     }
   }
 </script>
 
-<style lang="less" scoped>
+<style lang="less" >
     .tags-box {
         .order-info {
             padding-left: 10px;
         }
         padding: 0 30px !important;
+    }
+    .footer {
+        text-align: center;
+        margin-top: 20px;
+        .el-button {
+            width: 70px;
+            height: 30px;
+            padding: 0;
+        }
+        .saveBtn {
+            background: #FF4B57;
+            color: #fff;
+            border: 0;
+        }
     }
 </style>
