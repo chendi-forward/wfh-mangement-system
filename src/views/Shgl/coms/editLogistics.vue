e27a62b6ed9b13e9f04875caab5ae798a70f264c
panxuesen <13552198363@163.com>
2019-12-22 15:54:28 +0800

    订单详情

--- src/views/Shgl/coms/editLogistics.vue
+++ src/views/Shgl/coms/editLogistics.vue
@@ -3,12 +3,12 @@
     <div class="tags-box">
         <div class="tag-box">
           <span class="header-title">物流公司:</span>
-          <el-select v-model="selectKey" @change='selectChange' placeholder="请选择">
+          <el-select v-model="data.selectKey" @change='selectChange' placeholder="请选择">
             <el-option
               v-for="(item, index) in options"
               :key="index"
-              :label="item.label"
-              :value="item.value">
+              :label="item.name"
+              :value="item.logistics_id">
             </el-option>
           </el-select>
         </div>
@@ -16,40 +16,50 @@
           <span class="header-title">物流单号:</span>
           <el-input
             placeholder="请输入单号..."
-            v-model="logisticsId">
+            v-model="data.logisticsId">
           </el-input>
         </div>
     </div>
 </template>
 
 <script>
+import {logisticsManage} from 'API/Shgl'
   export default {
     props: {
       data: {
         type: Object,
         default: () => {
             return {
-                selectKey: '1',
+                selectKey: 1,
                 logisticsId: '',
-                options: [{
-                    value: '1',
-                    label: '顺丰速运'
-                }, {
-                    value: '2',
-                    label: '圆通快递'
-                }, {
-                    value: '3',
-                    label: '申通快递'
-                }]
+                
             }
         }
       }
     },
     data () {
       return {
+        options: [{
+            logistics_id: 1,
+            name: '顺丰速运'
+        }, {
+            logistics_id: 2,
+            name: '圆通快递'
+        }, {
+            logistics_id: 3,
+            name: '申通快递'
+        }]
       }
     },
+    created () {
+      this.logisticsManage()
+    },
     methods: {
+      logisticsManage () {
+        logisticsManage().then(res => {
+          this.options = res.data
+        })
+      },
       selectChange () {}
     }
   }
